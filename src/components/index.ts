c048b8431ab23bb8641cbfec30c996a5393513c7
Bruno Matheus <bmatheusc@gmail.com>
2022-08-19 15:03:31 -0300

    Adding new packages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as EventItem } from './EventItem';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
-export { default as EventItem } from './EventItem';
+export { default as Purchase } from './Purchase';
